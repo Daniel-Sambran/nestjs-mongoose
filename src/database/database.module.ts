790f86bfb71a6fa4ec46729986e412d9c0aab13c
Daniel Sambran <sambran65@gmail.com>
2022-11-12 10:35:56 -0300

    feat: arquitura base

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,11 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      // user: 'user',
-      // pass: 'pass'
-      dbName: 'example_db',
-    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://dsambran:atlas123@cluster0.jpo7gsh.mongodb.net',
+      {
+        // user: 'user',
+        // pass: 'pass'
+        dbName: 'example_db',
+      },
+    ),
   ],
 })
 export class DatabaseModule {}
